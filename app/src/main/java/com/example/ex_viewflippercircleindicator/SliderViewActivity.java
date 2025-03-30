b18f71766a6a79569a8c1cc78f5b0ff275b9ac64
Alro127 <nhuttan373@gmail.com>
2025-03-30 12:52:21 +0700

    first

---
+++ app/src/main/java/com/example/ex_viewflippercircleindicator/SliderViewActivity.java
@@ -0,0 +1,4 @@
+package com.example.ex_viewflippercircleindicator;
+
+public class SliderViewActivity {
+}
---
+++ app/src/main/java/com/example/ex_viewflippercircleindicator/ViewFlipperActivity.java
@@ -0,0 +1,62 @@
+package com.example.ex_viewflippercircleindicator;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewFlipperActivity extends AppCompatActivity {
+    ViewFlipper viewFlipperMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_flipper);
+        viewFlipperMain = findViewById(R.id.viewFlipperMain);
+        ActionViewFlipperMain();
+    }
+
+    // Hàm Flipper
+    private void ActionViewFlipperMain() {
+        List<String> arrayListFlipper = new ArrayList<>();
+        arrayListFlipper.add("http://app.iotstar.vn:8081/appfoods/flipper/quangcao.png");
+        arrayListFlipper.add("http://app.iotstar.vn:8081/appfoods/flipper/coffee.jpg");
+        arrayListFlipper.add("http://app.iotstar.vn:8081/appfoods/flipper/companypizza.jpg");
+        arrayListFlipper.add("http://app.iotstar.vn:8081/appfoods/flipper/themoingon.jpeg");
+        Toast.makeText(this, ""+arrayListFlipper.size(), Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < arrayListFlipper.size(); i++) {
+            String imageUrl = arrayListFlipper.get(i);
+            Log.d("ViewFlipper", "Loading image: " + imageUrl);
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext())
+                    .load(arrayListFlipper.get(i))
+                    .placeholder(R.drawable.ic_launcher_foreground) // Ảnh hiển thị khi đang tải
+                    .error(R.drawable.ic_launcher_background) // Ảnh hiển thị nếu tải lỗi
+                    .into(imageView);
+
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipperMain.addView(imageView);
+        }
+
+        viewFlipperMain.setFlipInterval(3000);
+        viewFlipperMain.setAutoStart(true);
+
+        // Thiết lập animation cho flipper
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipperMain.setInAnimation(slide_in);
+        viewFlipperMain.setOutAnimation(slide_out);
+    }
+}
+
